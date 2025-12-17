9ecf59391dd06edeb3a75c3e76a01c2b38b01848
ihmnoyon <noyonhasan320@gmail.com>
2025-12-17 21:01:01 +0600

    learnt get and post request api

--- main.py
+++ main.py
@@ -3,5 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Welcome to my first api"}
+
+
+
+@app.get("/message")
+def root():
+    return {"message": "how are you"}
+
+
+
+@app.post("/createposts")
+def create_posts():
+    return {"message":"successfully added your data"}
